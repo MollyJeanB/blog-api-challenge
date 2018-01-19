ab7e7d862f0dc3f0240144adc54bbc78c0edca54
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-01-19 13:13:31 -0800

    added sample data

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -6,4 +6,20 @@ const jsonParser = bodyParser.json();
 
 const { BlogPosts } = require("./models");
 
+BlogPosts.create(
+  "Today is a new day",
+  "What a lovely day it is",
+  "Molly Jean Bennett"
+);
+
+BlogPosts.create(
+  "New Thought",
+  "I have another thought about today, which is that it is not so lovely after all",
+  "Molly Jean Bennett"
+);
+
+router.get("/", (req, res) => {
+  res.json(BlogPosts.get());
+});
+
 module.exports = router;
